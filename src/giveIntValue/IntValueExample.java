d61a256199e0198b187f7cc95707601602fdabb1
rohitssalvi <rsalvi923@gmail.com>
2016-05-14 21:35:32 +0530

    Add files via upload

---
+++ src/giveIntValue/IntValueExample.java
@@ -0,0 +1,16 @@
+package giveIntValue;
+
+import java.util.BitSet;
+
+public class IntValueExample {
+	public static void main(String[] args) {
+		int first;
+		
+		System.out.println("1) String  data type can not to be store in int type varaible. ");
+		System.out.println();
+		System.out.println("2) Boolean  data type can not to be store in int type varaible. ");
+		
+		
+	}
+
+}
